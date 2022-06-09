43549d7d19287832b07aa1ed5ee323a4ce40683f
DCMMusic <102096177+DCMMusic@users.noreply.github.com>
2022-06-10 01:16:58 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5462750203:AAHghMHbSXZuh8tPc2raFIYnxCo3BztuLvY"
+TOKEN = "5502801468:AAGp1ar7RDOz36KeEzghFlzDx4Sjszyzoyg"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
